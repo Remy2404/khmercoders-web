59fd3f81de012ed211ebdf76966f5f7d85a9ffff
Visal .In <invisal@gmail.com>
2025-03-31 21:28:13 +0700

    add kol program section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,6 +145,55 @@ export default function LandingPage() {
         </div>
       </section>
 
+      <section className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 py-20 px-4 justify-center items-center">
+        <img
+          src="/assets/images/kol-program-md.png"
+          className="border-gray-800 border-b-8 w-full"
+        />
+        <div>
+          <div className="inline-flex items-center rounded-full border border-amber-500/30 bg-amber-500/10 px-3 py-1 text-sm text-amber-500 mb-4">
+            <span className="font-medium">Creator Program</span>
+          </div>
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Empowering{" "}
+            <span className="text-yellow-500">Technical Creators</span>
+          </h2>
+          <p className="text-gray-400 mb-6">
+            Our creator program supports technical content creators who share
+            knowledge, provide entertainment, and inspire the next generation to
+            pursue careers in STEM.
+          </p>
+          <p className="text-gray-400 mb-8">
+            We provide resources, mentorship, and a platform for creators to
+            reach wider audiences, while building a community of passionate
+            educators who make technical concepts accessible and engaging for
+            everyone.
+          </p>
+
+          <div className="flex flex-wrap gap-4">
+            <div className="bg-yellow-500/20 backdrop-blur-lg rounded-xl p-4 border border-yellow-500/80 shadow-lg w-[200px]">
+              <div className="text-2xl font-bold">10,000+</div>
+              <div className="text-sm">Subscribers</div>
+            </div>
+
+            <div className="bg-yellow-500/20 backdrop-blur-lg rounded-xl p-4 border border-yellow-500/80 shadow-lg w-[200px]">
+              <div className="text-2xl font-bold">1,000,000+</div>
+              <div className="text-sm">Views</div>
+            </div>
+
+            <div className="bg-yellow-500/20 backdrop-blur-lg rounded-xl p-4 border border-yellow-500/80 shadow-lg w-[200px]">
+              <div className="text-2xl font-bold">200,000+</div>
+              <div className="text-sm">Engagement</div>
+            </div>
+
+            <div className="bg-yellow-500/20 backdrop-blur-lg rounded-xl p-4 border border-yellow-500/80 shadow-lg w-[200px]">
+              <div className="text-2xl font-bold">200,000+</div>
+              <div className="text-sm">Engagement</div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       {/* Events Section */}
       <section id="events" className="bg-gray-900 py-20">
         <div className="container mx-auto px-4">
