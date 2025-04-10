31a577aad8123bf71e2568d5f77e94a8e9c123bd
Ave Aristov <chantepkik@gmail.com>
2025-04-10 14:12:10 +0700

    fix: adjest partners logo aspect radio ,improve acessability , remove invisible whitespace from partner data

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -13,7 +13,7 @@ export const partners: IPartner[] = [
   {
     name: "Bill24",
     description:
-      "​Bill24 streamlines bill payments, connecting businesses and customers through multiple channels.",
+      "Bill24 streamlines bill payments, connecting businesses and customers through multiple channels.",
     logo: "/assets/images/partners/bill24.png?height=200&width=400",
     website: "https://bill24.io/kh/",
     tags: [{ type: "Silver Sponsor", badge: 3 }],
@@ -21,7 +21,7 @@ export const partners: IPartner[] = [
   {
     name: "Binance Academy",
     description:
-      "​Binance Academy offers free, comprehensive education on blockchain and cryptocurrency topics. ",
+      "Binance Academy offers free, comprehensive education on blockchain and cryptocurrency topics.",
     logo: "/assets/images/partners/academy-h.png?height=200&width=400",
     website: "https://academy.binance.com",
     tags: [{ type: "Gold Sponsor", badge: 1 }],
@@ -29,7 +29,7 @@ export const partners: IPartner[] = [
   {
     name: "Plasgate",
     description:
-      "​PlasGate is Cambodia's largest authorized SMS gateway, connecting businesses to all major networks.",
+      "PlasGate is Cambodia's largest authorized SMS gateway, connecting businesses to all major networks.",
     logo: "/assets/images/partners/plusgate.webp?height=200&width=400",
     website: "https://plasgate.com/",
     tags: [
@@ -40,7 +40,7 @@ export const partners: IPartner[] = [
   {
     name: "Techbodia",
     description:
-      "​Techbodia is a leading Cambodian IT company offering customer-satisfaction-driven services. ",
+      "Techbodia is a leading Cambodian IT company offering customer-satisfaction-driven services.",
     logo: "/assets/images/partners/techbodia.png?height=200&width=400",
     website: "https://www.techbodia.com/",
     tags: [{ type: "Silver Sponsor", badge: 3 }],
@@ -48,7 +48,7 @@ export const partners: IPartner[] = [
   {
     name: "Jobify",
     description:
-      "​Jobify is Cambodia's leading IT job matching service, offering recruitment and staffing solutions to connect companies with skilled tech professionals.",
+      "Jobify is Cambodia's leading IT job matching service, offering recruitment and staffing solutions to connect companies with skilled tech professionals.",
     logo: "/assets/images/partners/jobify.jpg",
     website: "https://jobify.works",
     tags: [{ type: "Co-organizer", badge: 5 }],
@@ -56,7 +56,7 @@ export const partners: IPartner[] = [
   {
     name: "DataU",
     description:
-      "​DataU Academy offers live, interactive data science and AI training programs in Cambodia.",
+      "DataU Academy offers live, interactive data science and AI training programs in Cambodia.",
     logo: "/assets/images/partners/datau.png?height=200&width=400",
     website: "https://wedatau.org",
     tags: [{ type: "Co-organizer", badge: 5 }],
@@ -64,7 +64,7 @@ export const partners: IPartner[] = [
   {
     name: "Daun Penh Data Center",
     description:
-      "​DP CLOUD offers VPS, dedicated servers, web hosting, and other cloud services located in Cambodia.",
+      "DP CLOUD offers VPS, dedicated servers, web hosting, and other cloud services located in Cambodia.",
     logo: "/assets/images/partners/dpcloud_logo.png?height=200&width=400",
     website: "https://dpdatacenter.com/en",
     tags: [{ type: "Silver Sponsor", badge: 3 }],
@@ -72,7 +72,7 @@ export const partners: IPartner[] = [
   {
     name: "Cambodian Network Exchange",
     description:
-      "​Cambodian Network Exchange (CNX) is an open, neutral internet exchange in Cambodia. ",
+      "Cambodian Network Exchange (CNX) is an open, neutral internet exchange in Cambodia.",
     logo: "/assets/images/partners/cnx.png?height=200&width=400",
     website: "https://cnx.net.kh/",
     tags: [{ type: "Silver Sponsor", badge: 1 }],
@@ -80,7 +80,7 @@ export const partners: IPartner[] = [
   {
     name: "Sabay TeKh",
     description:
-      "​Sabay Cloud offers cloud-based video streaming, storage, and transcoding services in Cambodia.",
+      "Sabay Cloud offers cloud-based video streaming, storage, and transcoding services in Cambodia.",
     logo: "/assets/images/partners/sabay-tekh.png?height=200&width=400",
     website: "https://cloud.sabay.com/",
     tags: [{ type: "Silver Sponsor", badge: 1 }],
@@ -88,7 +88,7 @@ export const partners: IPartner[] = [
   {
     name: "AI Farm",
     description:
-      "​AI Farm Robotic Factory, a subsidiary of Khamsa Group of Businesses (KGB), is a technology-driven company based in Phnom Penh, Cambodia.",
+      "AI Farm Robotic Factory, a subsidiary of Khamsa Group of Businesses (KGB), is a technology-driven company based in Phnom Penh, Cambodia.",
     logo: "/assets/images/partners/aifarm.png?height=200&width=400",
     website: "https://aifarm.dev/",
     tags: [{ type: "Silver Sponsor", badge: 2 }],
@@ -96,7 +96,7 @@ export const partners: IPartner[] = [
   {
     name: "Dreamlab",
     description:
-      "​DreamsLAB is a Cambodian software solutions company specializing in custom software, mobile apps, cloud services, AI, and blockchain integration. ",
+      "DreamsLAB is a Cambodian software solutions company specializing in custom software, mobile apps, cloud services, AI, and blockchain integration.",
     logo: "/assets/images/partners/dreamslab.svg?height=200&width=400",
     website: "https://www.dreamslab.dev/",
     tags: [
@@ -115,7 +115,7 @@ export const partners: IPartner[] = [
   {
     name: "Raintree",
     description:
-      "​Raintree is Cambodia's first creative office development, offering flexible workspaces and event venues in Phnom Penh's central business district",
+      "Raintree is Cambodia's first creative office development, offering flexible workspaces and event venues in Phnom Penh's central business district",
     logo: "/assets/images/partners/raintree.png",
     website: "https://www.raintreecambodia.com/",
     tags: [{ type: "Venue", badge: 1 }],
@@ -129,3 +129,4 @@ export const partners: IPartner[] = [
     tags: [{ type: "Venue", badge: 1 }],
   },
 ];
+
