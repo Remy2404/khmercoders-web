f4c736b3c60f38da53013b1ab49a263a90a6d12f
Visal .In <invisal@gmail.com>
2025-06-16 20:27:12 +0700

    refactor: remove duplicate "use client" directive from ProfileTrackingComponent

--- src/app/[username]/tracker.tsx
+++ src/app/[username]/tracker.tsx
@@ -1,9 +1,8 @@
+"use client";
 import { trackProfileVisitAction } from "@/actions/track";
 import { useEffect } from "react";
 
 export function ProfileTrackingComponent({ userId }: { userId: string }) {
-  "use client";
-
   useEffect(() => {
     trackProfileVisitAction({ userId }).then().catch();
   }, [userId]);
