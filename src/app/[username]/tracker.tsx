f0d4b52bd290829c0017eae2558ba93a52932160
Visal .In <invisal@gmail.com>
2025-06-17 16:12:45 +0700

    fix: correct condition in useEffect to ensure userId is present before tracking profile visits

--- src/app/[username]/tracker.tsx
+++ src/app/[username]/tracker.tsx
@@ -4,7 +4,8 @@ import { useEffect } from "react";
 
 export function ProfileTrackingComponent({ userId }: { userId: string }) {
   useEffect(() => {
-    if (typeof window !== "undefined" && !userId) {
+    if (typeof window !== "undefined" && userId) {
+      console.log("Tracking profile visit for user:", userId);
       trackProfileVisitAction({ userId, referrer: window.document.referrer })
         .then()
         .catch();
