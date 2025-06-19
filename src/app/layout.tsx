7d6d7b1c9350edcd212743b4f14ec27fc9603bb3
masayaShinoda <masayashida@gmail.com>
2025-06-19 18:40:18 +0700

    proper favicon display across browsers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning={true}>
       <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
