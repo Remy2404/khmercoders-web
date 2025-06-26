5f946f30169a5561fe0980b5effef2c704f390ee
Visal .In <invisal@gmail.com>
2025-06-26 16:07:24 +0700

    feat: replace empty scheduled function with Cloudflare scheduled handler

--- custom-worker.ts
+++ custom-worker.ts
@@ -1,9 +1,9 @@
 import { default as handler } from './.open-next/worker.js';
+import { handleCloudflareScheduled } from './src/cron';
 
 export default {
   fetch: handler.fetch,
-
-  async scheduled() {},
+  scheduled: handleCloudflareScheduled,
 } satisfies ExportedHandler<CloudflareEnv>;
 
 // The re-export is only required if your app uses the DO Queue and DO Tag Cache
