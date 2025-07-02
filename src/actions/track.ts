a496686adb5235d2d2c8fd6a371defd197de6110
Visal .In <invisal@gmail.com>
2025-07-02 09:26:00 +0700

    feat: tracking article view

--- src/actions/track.ts
+++ src/actions/track.ts
@@ -7,8 +7,12 @@ import { headers } from 'next/headers';
 export async function trackProfileVisitAction({
   userId,
   referrer,
+  eventType = 'pageview',
+  articleId,
 }: {
   userId: string;
+  eventType: 'pageview' | 'article';
+  articleId?: string;
   referrer?: string;
 }) {
   const { cf, env } = await getCloudflareContext({ async: true });
@@ -25,15 +29,20 @@ export async function trackProfileVisitAction({
 
     const uniqueSessionId = crypto.createHash('sha1').update(ipAddress).digest('hex');
 
+    // eventSecondaryId provides additional context specific to the event type
+    // For example, with 'article' events, it contains the article ID
+    const eventSecondaryId = articleId || '';
+
     env.PROFILE_ANALYTICS.writeDataPoint({
       indexes: [userId],
       blobs: [
-        'pageview', // blob1, event type
+        eventType, // blob1, event type
         countryCode || '', // blob2
         deviceType, // blob3
         uniqueSessionId, // blob4
         referrer || '', // blob5
         path, // blob6
+        eventSecondaryId || '', // blob7, event secondary id (optional)
       ],
     });
   }
