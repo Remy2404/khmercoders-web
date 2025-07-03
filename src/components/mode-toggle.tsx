98beaff40f880d669abac9d9cc6aa7d8ba46ce46
Rathpanha <rath.panha.rp@gmail.com>
2025-07-03 13:31:09 +0700

    feat: add mode toggle into navbar

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,21 +1,21 @@
 'use client';
 
-import { Button } from '@/components/generated/button';
+import { Button, ButtonProps } from '@/components/generated/button';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-export function ModeToggle() {
+export function ModeToggle({ variant = 'outline' }: { variant?: ButtonProps['variant'] }) {
   const { resolvedTheme, setTheme } = useTheme();
 
   return (
     <Button
       type="button"
-      variant="outline"
+      variant={variant}
       size="icon"
       onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
-      <Sun className="absolute size-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-      <Moon className="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Sun className="absolute size-6 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Moon className="size-6 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
