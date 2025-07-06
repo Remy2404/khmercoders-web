f2094e305ce1067a9efabcf8fdc756c7b77c2700
Visal .In <invisal@gmail.com>
2025-07-06 11:20:15 +0700

    Add getDBFromEnvironment

--- src/libs/db/index.ts
+++ src/libs/db/index.ts
@@ -6,19 +6,26 @@ import * as schema from './schema';
 
 export let db: DrizzleD1Database<typeof schema> | null = null;
 
-/**
- * Get the D1 database instance. Override is used primary for cron which
- * it does not have getCloudflareContext access.
- *
- * @param overrideEnv Override the Cloudflare environment if needed.
- * @returns
- */
-export const getDB = async (overrideEnv?: CloudflareEnv) => {
+export const getDB = async () => {
   if (db) {
     return db;
   }
 
-  const env = overrideEnv || (await getCloudflareContext({ async: true })).env;
+  const { env } = await getCloudflareContext({ async: true });
+
+  if (!env.DB) {
+    throw new Error('D1 database not found');
+  }
+
+  db = drizzle(env.DB, { schema, logger: true });
+
+  return db;
+};
+
+export const getDBFromEnvironment = (env: CloudflareEnv) => {
+  if (db) {
+    return db;
+  }
 
   if (!env.DB) {
     throw new Error('D1 database not found');
