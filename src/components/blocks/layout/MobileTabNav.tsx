9d3d3e9f83b65dcc1908c7d8fcc57a436fde1a18
Visal .In <invisal@gmail.com>
2025-07-24 19:27:46 +0700

    feat: implement MobileSheetContent component and integrate it into MobileTabNavigation

--- src/components/blocks/layout/MobileTabNav.tsx
+++ src/components/blocks/layout/MobileTabNav.tsx
@@ -7,11 +7,10 @@ import {
 } from '@/components/generated/sheet';
 import { AlignJustify, HomeIcon, PlusIcon, SearchIcon } from 'lucide-react';
 import Link from 'next/link';
-import { useNavigationRoutes } from './NavigationRoute';
+import { MobileSheetContent } from './MobileSheetContent';
 
 export function MobileTabNavigation() {
   const tabButtonClass = 'flex text-center p-3 items-center justify-center focus:outline-none';
-  const routes = useNavigationRoutes();
 
   return (
     <div className="md:hidden fixed bottom-0 left-0 w-screen bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 z-50">
@@ -34,18 +33,8 @@ export function MobileTabNavigation() {
           <SheetContent>
             <SheetTitle>Khmer Coders</SheetTitle>
             <SheetDescription></SheetDescription>
-            <div className="flex flex-col -m-2 my-4">
-              {routes.map((route, routeIdx) => {
-                if (route === '---') {
-                  return <hr key={routeIdx} className="my-2" />;
-                }
-
-                return (
-                  <Link key={routeIdx} className="justify-start p-2" href={route.path}>
-                    {route.title}
-                  </Link>
-                );
-              })}
+            <div className="mt-4">
+              <MobileSheetContent />
             </div>
           </SheetContent>
         </Sheet>
